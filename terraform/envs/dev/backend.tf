6ed6cf5daa1cbdd9f98c8f599e497a26d287deb4
Walter <walterfontoura82@gmail.com>
2026-07-03 08:33:18 -0300

    fix sandbox aws account references

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloud-operations-portal-tfstate-8706a97d"
+    bucket         = "cloud-operations-portal-tfstate-e2aed0e7"
     key            = "envs/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cloud-operations-portal-tf-locks"
