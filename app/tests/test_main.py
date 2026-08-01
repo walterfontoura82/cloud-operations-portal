47f813ad8230bf0e52d3513ae1b5a72a8149f1eb
Walter <walterfontoura82@gmail.com>
2026-08-01 20:48:03 -0300

    add application version endpoint

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -17,3 +17,10 @@ def test_root():
 
     assert response.status_code == 200
     assert response.json() == {"message": "Cloud Operations Portal API"}
+
+
+def test_version():
+    response = client.get("/version")
+
+    assert response.status_code == 200
+    assert response.json() == {"version": "1.0.0"}
